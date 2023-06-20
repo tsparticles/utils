be5841584f4b3a6d6b011031548e8c1929b05c2c
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-06-21 00:58:41 +0200

    feat: added eslint-plugin-import for enforcing .js at the end of every import

--- packages/eslint-config/eslint-config.js
+++ packages/eslint-config/eslint-config.js
@@ -4,161 +4,100 @@ module.exports = {
     parserOptions: {
         project: "./tsconfig.json"
     },
-    plugins: ["@typescript-eslint", "jsdoc", "eslint-plugin-tsdoc"],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:jsdoc/recommended-typescript",
-        "prettier"
-    ],
+    plugins: ["@typescript-eslint", "import", "jsdoc", "eslint-plugin-tsdoc"],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended", "plugin:jsdoc/recommended-typescript", "prettier", "plugin:import/recommended", "plugin:import/typescript"],
     rules: {
         "@typescript-eslint/ban-types": "warn",
         "@typescript-eslint/comma-spacing": ["error"],
-        "@typescript-eslint/consistent-generic-constructors": [
-            "error",
-            "constructor"
-        ],
+        "@typescript-eslint/consistent-generic-constructors": ["error", "constructor"],
         "@typescript-eslint/consistent-type-exports": ["error"],
         "@typescript-eslint/consistent-type-imports": ["error"],
         "@typescript-eslint/explicit-function-return-type": "error",
-        "@typescript-eslint/explicit-member-accessibility": [
-            "error",
-            {
-                accessibility: "no-public"
-            }
-        ],
-        "@typescript-eslint/no-explicit-any": "error",
-        //"@typescript-eslint/no-extra-parens": [ "error" ],
+        "@typescript-eslint/explicit-member-accessibility": ["error", {
+            accessibility: "no-public"
+        }],
+        "@typescript-eslint/no-explicit-any": "error", //"@typescript-eslint/no-extra-parens": [ "error" ],
         "@typescript-eslint/no-inferrable-types": "error",
-        "@typescript-eslint/member-ordering": [
-            "error",
-            {
-                default: {
-                    memberTypes: [
-                        // Index signature
-                        "signature",
+        "@typescript-eslint/member-ordering": ["error", {
+            default: {
+                memberTypes: [// Index signature
+                    "signature",
 
-                        // Fields
-                        "public-static-field",
-                        "protected-static-field",
-                        "private-static-field",
+                    // Fields
+                    "public-static-field", "protected-static-field", "private-static-field",
 
-                        "public-decorated-field",
-                        "protected-decorated-field",
-                        "private-decorated-field",
+                    "public-decorated-field", "protected-decorated-field", "private-decorated-field",
 
-                        "public-instance-field",
-                        "protected-instance-field",
-                        "private-instance-field",
+                    "public-instance-field", "protected-instance-field", "private-instance-field",
 
-                        "public-abstract-field",
-                        "protected-abstract-field",
+                    "public-abstract-field", "protected-abstract-field",
 
-                        "public-field",
-                        "protected-field",
-                        "private-field",
+                    "public-field", "protected-field", "private-field",
 
-                        "static-field",
-                        "instance-field",
-                        "abstract-field",
+                    "static-field", "instance-field", "abstract-field",
 
-                        "decorated-field",
+                    "decorated-field",
 
-                        "field",
+                    "field",
 
-                        // Constructors
-                        "public-constructor",
-                        "protected-constructor",
-                        "private-constructor",
+                    // Constructors
+                    "public-constructor", "protected-constructor", "private-constructor",
 
-                        "constructor",
+                    "constructor",
 
-                        // Getters & Setters
-                        ["public-static-get", "public-static-set"],
-                        ["protected-static-get", "protected-static-set"],
-                        ["private-static-get", "private-static-set"],
+                    // Getters & Setters
+                    ["public-static-get", "public-static-set"], ["protected-static-get", "protected-static-set"], ["private-static-get", "private-static-set"],
 
-                        ["public-decorated-get", "public-decorated-set"],
-                        ["protected-decorated-get", "protected-decorated-set"],
-                        ["private-decorated-get", "private-decorated-set"],
+                    ["public-decorated-get", "public-decorated-set"], ["protected-decorated-get", "protected-decorated-set"], ["private-decorated-get", "private-decorated-set"],
 
-                        ["public-instance-get", "public-instance-set"],
-                        ["protected-instance-get", "protected-instance-set"],
-                        ["private-instance-get", "private-instance-set"],
+                    ["public-instance-get", "public-instance-set"], ["protected-instance-get", "protected-instance-set"], ["private-instance-get", "private-instance-set"],
 
-                        ["public-abstract-get", "public-abstract-set"],
-                        ["protected-abstract-get", "protected-abstract-set"],
+                    ["public-abstract-get", "public-abstract-set"], ["protected-abstract-get", "protected-abstract-set"],
 
-                        ["public-get", "public-set"],
-                        ["protected-get", "protected-set"],
-                        ["private-get", "private-set"],
+                    ["public-get", "public-set"], ["protected-get", "protected-set"], ["private-get", "private-set"],
 
-                        ["static-get", "static-set"],
-                        ["instance-get", "instance-set"],
-                        ["abstract-get", "abstract-set"],
+                    ["static-get", "static-set"], ["instance-get", "instance-set"], ["abstract-get", "abstract-set"],
 
-                        ["decorated-get", "decorated-set"],
+                    ["decorated-get", "decorated-set"],
 
-                        ["get", "set"],
+                    ["get", "set"],
 
-                        // Methods
-                        "public-static-method",
-                        "protected-static-method",
-                        "private-static-method",
+                    // Methods
+                    "public-static-method", "protected-static-method", "private-static-method",
 
-                        "public-decorated-method",
-                        "protected-decorated-method",
-                        "private-decorated-method",
+                    "public-decorated-method", "protected-decorated-method", "private-decorated-method",
 
-                        "public-instance-method",
-                        "protected-instance-method",
-                        "private-instance-method",
+                    "public-instance-method", "protected-instance-method", "private-instance-method",
 
-                        "public-abstract-method",
-                        "protected-abstract-method",
+                    "public-abstract-method", "protected-abstract-method",
 
-                        "public-method",
-                        "protected-method",
-                        "private-method",
+                    "public-method", "protected-method", "private-method",
 
-                        "static-method",
-                        "instance-method",
-                        "abstract-method",
+                    "static-method", "instance-method", "abstract-method",
 
-                        "decorated-method",
+                    "decorated-method",
 
-                        "method"
-                    ],
-                    order: "alphabetically"
-                }
+                    "method"], order: "alphabetically"
             }
-        ],
+        }],
         "@typescript-eslint/no-unnecessary-type-arguments": "error",
         "@typescript-eslint/no-unnecessary-type-assertion": "error",
-        "@typescript-eslint/no-unused-vars": [
-            "error",
-            {
-                argsIgnorePattern: "^_",
-                caughtErrorsIgnorePattern: "^_",
-                destructuredArrayIgnorePattern: "^_",
-                varsIgnorePattern: "^_"
-            }
-        ],
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+            destructuredArrayIgnorePattern: "^_",
+            varsIgnorePattern: "^_"
+        }],
         "@typescript-eslint/no-var-requires": "error",
         "@typescript-eslint/prefer-readonly": ["error"],
-        "comma-spacing": "off",
-        //"no-extra-parens": "off",
-        "sort-imports": [
-            "error",
-            {
-                allowSeparatedGroups: false,
-                ignoreCase: false,
-                ignoreDeclarationSort: false,
-                ignoreMemberSort: false,
-                memberSyntaxSortOrder: ["none", "all", "multiple", "single"]
-            }
-        ],
+        "comma-spacing": "off", //"no-extra-parens": "off",
+        "sort-imports": ["error", {
+            allowSeparatedGroups: false,
+            ignoreCase: false,
+            ignoreDeclarationSort: false,
+            ignoreMemberSort: false,
+            memberSyntaxSortOrder: ["none", "all", "multiple", "single"]
+        }],
         "tsdoc/syntax": "warn"
     }
 };
